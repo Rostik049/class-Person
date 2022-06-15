09552e778fad85138f7022e0db0b0bc020daa0d8
Rostik049 <90641608+Rostik049@users.noreply.github.com>
2022-06-15 14:24:16 +0300

    Update class Person.cpp

--- class Person/class Person.cpp
+++ class Person/class Person.cpp
@@ -8,14 +8,6 @@ using namespace std;
 
 int main()
 {
-    // double TOS;int NOF;
-    // int *nof_ptr = &NOF;
-    // sd.add_soldier(Weapon::machine_gun,TOS,NOF,Service::contract_service,Rank::soldier);
-    // sd.add_soldier(Weapon::machine,TOS,NOF,Service::contract_service,Rank::soldier);
-    // sd.del(0);
-    // sd.del(1);
-    // sd.fight();
-    // sd.print();
     Person_System sd;
     sd.menu();
     return 0;
